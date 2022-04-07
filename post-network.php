bc4227db0a39330b74df5c414b848c12eca6d88c
Nick Chomey <nick_chomey@hotmail.com>
2022-04-07 10:09:19 -0600

    adding hooks, other edits and improvements
    Signed-off-by: Nick Chomey <nick_chomey@hotmail.com>

--- post-network.php
+++ post-network.php
@@ -6,7 +6,7 @@ Description: Visualize the relationship between articles based on internal links
 Author: Naoto Onodera
 Author URI: https://notondr.com/
 Plugin URI: https://notondr.com/post-network/
-Version: 1.2.0
+Version: 1.3.0
 Text Domain: post-network
 Domain Path: /languages
 */
@@ -37,5 +37,6 @@ add_action( 'admin_enqueue_scripts', 'pn_theme_enqueue_styles' );
  */
 function pn_theme_enqueue_scripts() {
 	wp_enqueue_script( 'visjs', plugins_url( '/js/vis-network.min.js', __FILE__ ) );
+	wp_enqueue_script( 'pn', plugins_url( '/js/pn.js', __FILE__ ) );
 }
 add_action( 'admin_enqueue_scripts', 'pn_theme_enqueue_scripts' );
